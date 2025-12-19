b4f0f4501c1fef6371f350325262855111818483
phLinux <paulohtolotti@alumni.usp.br>
2025-12-19 15:16:52 -0300

    first commit

---
+++ src/main/java/com/phsoft/phcommerce/PhcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.phsoft.phcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phsoft/phcommerce/PhcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phsoft.phcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
