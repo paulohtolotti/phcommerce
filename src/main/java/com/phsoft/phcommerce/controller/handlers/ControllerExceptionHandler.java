2d27323b4ae1b1af43d088502c28af00c0c33130
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2026-01-12 16:26:04 -0300

    feat: inclusão de um método para interceptar exceptions de banco de dados

--- src/main/java/com/phsoft/phcommerce/controller/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/phsoft/phcommerce/controller/handlers/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.phsoft.phcommerce.dto.CustomError;
+import com.phsoft.phcommerce.services.exception.DatabaseException;
 import com.phsoft.phcommerce.services.exception.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,6 +26,21 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    /**
+     * Método que lida com exceções do tipo recurso não encontrado.
+     * Retorna uma ResponseEntity parametrizada com o Dto de error customizado.
+     * @param e Exceção personalizada lançada pelo service
+     * @param request objeto representando a requisição
+     * @return
+     */
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> databaseException(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
