1ad2973650d4f1b5b0709544c2297251f17f6bf5
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2026-01-12 10:19:06 -0300

    feat: ajuste dos métodos para retornar um ResponseEntity

--- src/main/java/com/phsoft/phcommerce/controller/ProductController.java
+++ src/main/java/com/phsoft/phcommerce/controller/ProductController.java
@@ -1,19 +1,20 @@
 package com.phsoft.phcommerce.controller;
 
 import com.phsoft.phcommerce.dto.ProductDTO;
-import com.phsoft.phcommerce.entities.Product;
 import com.phsoft.phcommerce.services.ProductService;
 
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -32,8 +33,9 @@ public class ProductController {
      * @return
      */
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     /**
@@ -42,19 +44,26 @@ public class ProductController {
      * @return List<Page>: listagem paginada
      */
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable page) {
-        return service.findAll(page);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable page) {
+        Page<ProductDTO> dto =  service.findAll(page);
+        return ResponseEntity.ok(dto);
     }
 
     /**
      * Método que realiza a inserção de um novo produto no banco de dados.
-     * 
+     * Insere a URI do recurso criado no cabeçalho da requisição, usando o objeto URI.
      * @param dto objeto dto instanciado a partir do corpo da requisição 
      * @return objeto dto atualizado após ser inserido no BD
      */
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO dto) {
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+
+        //Boa prática para a resposta da criação.
+        // Insere no cabeçalho da respsota a URI do recurso
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 
