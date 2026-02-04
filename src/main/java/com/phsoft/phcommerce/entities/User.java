d1e4dc619cf00836956304c0e05812391d1f3f45
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2026-02-04 13:04:13 -0300

    feat: implementação do modelo de domínio para contorle de acesso

--- src/main/java/com/phsoft/phcommerce/entities/User.java
+++ src/main/java/com/phsoft/phcommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.phsoft.phcommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name="tb_user")
@@ -23,6 +21,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
