4522cda5fe959839a49b22b79fd8b339f55f1989
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2026-02-05 15:25:49 -0300

    feat: criação de uma projection para query SQL para autenticação de usuário

--- src/main/java/com/phsoft/phcommerce/projections/AuthProjection.java
+++ src/main/java/com/phsoft/phcommerce/projections/AuthProjection.java
@@ -3,6 +3,7 @@ package com.phsoft.phcommerce.projections;
 public interface AuthProjection {
     Long getUserId(); // as userId
     String getEmail(); // as email
+    String getPassword();
     Long getRoleId(); // as roleId
     String getAuthority(); // as authority
 }
