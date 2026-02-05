6a7031308bedf5da0a4b393e5d6570a422cac3d8
Paulo Tolotti <paulohtolotti@alumni.usp.br>
2026-02-05 13:35:26 -0300

    feat: implementação das interfaces GrantedAuthority e UserDetails do spring security

--- src/main/java/com/phsoft/phcommerce/entities/User.java
+++ src/main/java/com/phsoft/phcommerce/entities/User.java
@@ -1,13 +1,15 @@
 package com.phsoft.phcommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name="tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -80,8 +82,19 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 
     public void setPassword(String password) {
